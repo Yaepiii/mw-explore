1fd052b6c22434770206e209fd8017bb6c51fc97
Yanpeng Jia <75295024+Yaepiii@users.noreply.github.com>
2023-01-15 15:17:09 +0800

    Add files via upload

---
+++ explore_k/include/explore/frontier_search.h
@@ -0,0 +1,110 @@
+#ifndef FRONTIER_SEARCH_H_
+#define FRONTIER_SEARCH_H_
+
+#include <costmap_2d/costmap_2d.h>
+
+namespace frontier_exploration
+{
+/**
+ * @brief Represents a frontier
+ */
+struct Frontier {
+  std::uint32_t size;
+  double min_distance;
+  double information;
+  double cost;
+  geometry_msgs::Point initial;
+  geometry_msgs::Point centroid;
+  geometry_msgs::Point middle;
+  std::vector<geometry_msgs::Point> points;
+};
+
+/**
+ * @brief Thread-safe implementation of a frontier-search task for an input
+ * costmap.
+ */
+class FrontierSearch
+{
+public:
+  FrontierSearch()
+  {
+  }
+
+  /**
+   * @brief Constructor for search task
+   * @param costmap Reference to costmap data to search.
+   * @param potential_scale Weight related to minimum distance 
+   * @param gain_scale Weight related to frontier size
+   * @param information_scale Weight related to information gain
+   * @param min_frontier_size Consider the frontier as the minimum frontier size
+   * @param information_r Information gain detection radius
+   */
+  FrontierSearch(costmap_2d::Costmap2D* costmap, double potential_scale,
+                 double gain_scale,double information_scale,
+                 double min_frontier_size, double information_r);
+
+  /**
+   * @brief Runs search implementation, outward from the start position
+   * @param position Initial position to search from
+   * @return List of frontiers, if any
+   */
+  std::vector<Frontier> searchFrom(geometry_msgs::Point position);
+
+  /**
+   * @brief Dynamic reconfigure parameter update
+   */
+  void configUpdate(double min_frontier_size, double potential_scale,
+                    double gain_scale, double information_scale, double information_r);
+
+protected:
+  /**
+   * @brief Starting from an initial cell, build a frontier from valid adjacent
+   * cells
+   * @param initial_cell Index of cell to start frontier building
+   * @param reference Reference index to calculate position from
+   * @param frontier_flag Flag vector indicating which cells are already marked
+   * as frontiers
+   * @return new frontier
+   */
+  Frontier buildNewFrontier(unsigned int initial_cell, unsigned int reference,
+                            std::vector<bool>& frontier_flag);
+
+  /**
+   * @brief isNewFrontierCell Evaluate if candidate cell is a valid candidate
+   * for a new frontier.
+   * @param idx Index of candidate cell
+   * @param frontier_flag Flag vector indicating which cells are already marked
+   * as frontiers
+   * @return true if the cell is frontier cell
+   */
+  bool isNewFrontierCell(unsigned int idx,
+                         const std::vector<bool>& frontier_flag);
+
+  /**
+   * @brief computes frontier cost
+   * @details cost function is defined by potential_scale and gain_scale
+   *
+   * @param frontier frontier for which compute the cost
+   * @return cost of the frontier
+   */
+  double frontierCost(const Frontier& frontier);
+
+  /**
+   * @brief compute information gain
+   * @param point Frontier for which compute the information gain
+   * @param r Information gain radius
+   * @return information gain of the frontier
+   */
+  double informationGain(geometry_msgs::Point point, double r);
+
+private:
+  costmap_2d::Costmap2D* costmap_;
+  unsigned char* map_;
+  unsigned int size_x_, size_y_;
+  double potential_scale_, gain_scale_, information_scale_;
+  double min_frontier_size_;
+  double information_r_;
+  double h_rad = 3.0, h_gain = 2.0;
+};
+}
+#endif
